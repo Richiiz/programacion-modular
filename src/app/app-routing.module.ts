1d15d01e3fc394a4cca4fc1b7e4244eb920e43ca
Richiiz <ricardo.rodriguez@librerialeo.com.mx>
2022-12-26 15:43:36 -0600

    Creando rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,44 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { CategoryComponent } from './pages/category/category.component';
+import { MycartComponent } from './pages/mycart/mycart.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { RecoveryComponent } from './pages/recovery/recovery.component';
 
-const routes: Routes = [];
+// por cada ruta vamos a tener un render unico
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'Not-Found',
+    component: NotFoundComponent
+  },
+  {
+    path: 'category',
+    component: CategoryComponent
+  },
+  {
+    path: 'My-Cart',
+    component: MycartComponent
+  },
+  {
+    path: 'Login',
+    component: LoginComponent
+  },
+  {
+    path: 'Register',
+    component: RegisterComponent
+  },
+  {
+    path: 'Recovery',
+    component: RecoveryComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
