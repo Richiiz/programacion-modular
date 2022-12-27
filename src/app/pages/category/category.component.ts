ddf7003211e8098d7d1c13b0635a7896271800bb
Richiiz <ricardo.rodriguez@librerialeo.com.mx>
2022-12-27 11:43:48 -0600

    Evitando doble subscribe

--- src/app/pages/category/category.component.ts
+++ src/app/pages/category/category.component.ts
@@ -1,12 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+// con switchMap evitamos
+import { switchMap } from 'rxjs/operators';
 // importamos el archivo
 import { ProductsService } from './../../services/products.service'
 import { Product } from '../../models/product.model';
 
 @Component({
   selector: 'app-category',
-  templateUrl: './category.component.html',
+  // le añadimos la linea de codigo del html ya que al ser solo una linea es mucho para un html, en caso de que haya mas logica
+  // ahora si se tendria que implementar en un html
+  template: `<app-products [products]="products" (loadMore)="onLoadMore()"></app-products>`,
   styleUrls: ['./category.component.scss']
 })
 export class CategoryComponent implements OnInit {
@@ -26,19 +30,30 @@ export class CategoryComponent implements OnInit {
 
   ngOnInit(): void {
     // cada ves que se envie un nuevo id se obtendran esos eventos por medio de paramMap
-    this.route.paramMap.subscribe(params => {
-      // lo que esta entre parentesis debe conincidir con el path category que esta en app-routing.module.ts por ejemplo
-      this.categoryId = params.get('id');
-      if(this.categoryId){
-        // generamos el request para obteber la funcion
-        // lo pnemos dentro de un if ya que aqui verificamos que la categoria no sea null
-        this.productsService.getByCategory(this.categoryId, this.limit, this.offset)
-          .subscribe( data => {
+    this.route.paramMap
+    .pipe(
+      switchMap(params => {
+         this.categoryId = params.get('id');
+        if(this.categoryId){
+          return this.productsService.getByCategory(this.categoryId, this.limit, this.offset)
+        }
+        return [];
+      })
+    )
+    .subscribe((data) => {
             // le decimos  que los productos van a ser igual a la data que encuentre con esa info
             this.products = data;
-          })
+          });
       }
-    })
-  }
+      onLoadMore() {
+        if (this.categoryId) {
+          this.productsService
+          .getByCategory(this.categoryId, this.limit, this.offset)
+          .subscribe((data) => {
+            this.products = this.products.concat(data);
+            this.offset += this.limit;
+          })
+        }
+      }
 
 }
