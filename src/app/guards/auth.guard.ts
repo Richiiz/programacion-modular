f496c71beb9afb93fde5d0e09a2c45c35682a009
Richiiz <ricardo.rodriguez@librerialeo.com.mx>
2022-12-28 17:15:14 -0600

    Implementando redirects

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
 
@@ -10,15 +11,21 @@ import { TokenService } from './../services/token.service'
 export class AuthGuard implements CanActivate {
 
 constructor(
-  private tokenService: TokenService
+  private tokenService: TokenService,
+  private router: Router
 ) {}
 
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
       const token = this.tokenService.getToken();
+      if(!token){
+        // si no existe el token, entonces retorna un false impidiendo acceso y tambien nos redirecciona al home
+        this.router.navigate(['/home']);
+        return false;
+      }
       // si nuestro guardian comprueba que existe token, entonces nos permite el acceso
-      return token ? true : false;
+      return true;
   }
 
 }
